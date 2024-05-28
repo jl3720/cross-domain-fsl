82e246fb33437b2c54719438435ba38858f21ff1
jl3720 <jameszcliu@gmail.com>
2024-05-28 18:18:28 +0200

    Restructured repo as package. Added setup.py and __init__.py files.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cross-domain-fsl",
+    version="0.0.1",
+    description="Master Thesis on Cross-domain Few-shot Learning",
+    author="James Liu",
+    packages=find_packages("cross_domain_fsl"),
+)
