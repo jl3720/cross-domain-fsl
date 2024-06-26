3c26dab313c5817bc8d67a65d9d820c8a2de88c9
James Liu <jameszcliu@gmail.com>
2024-06-26 11:51:49 +0200

    Added euler script for finetuning fc heads.

---
+++ scripts/euler_finetune_foundation_fc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH  --output=logs/%j.out
+#SBATCH  --gpus=1
+#SBATCH  --gres=gpumem:11000m
+#SBATCH  --mem-per-cpu=4g
+#SBATCH  --ntasks=1
+#SBATCH  --cpus-per-task=8
+
+source /cluster/home/jamliu/.bashrc  # load cvl settings and module envs
+source /cluster/home/jamliu/virtualenvs/cd_fsl/bin/activate  # venv
+
+# Get the current time
+time=`date +%Y%m%d`
+echo $(date +%H%M%S-%Y%m%d)
+
+# Run the pipeline
+# N.B. passes all args to scripts, only works because scripts accept same args
+python -u cross_domain_fsl/baselines/finetune_foundation_fc.py "$@"
+echo $(date +%H%M%S-%Y%m%d)
+
+# Organise the logs under date time
+new_dir="./logs/finetune_fc/${time}"
+mkdir -p ${new_dir}
+mv ./logs/${SLURM_JOB_ID}.out ${new_dir}
